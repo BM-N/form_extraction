a7dcf27da1bda318e17ead0f5544ee8e74e3435c
BM-N <bru_nunes10@hotmail.com>
2025-03-04 17:01:06 -0300

    minor file adjustments

--- src/main.py
+++ src/main.py
@@ -16,14 +16,18 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
-# Já está mandando o texto extraido para o banco
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to the OCR API"}
+
+# Post method to send the extract text to the sql db
 @app.post('/uploadimages/', response_model=NyxSQL)
 async def read_img(file: Annotated[UploadFile, File(description='Upload an image:')],
              session: Session = Depends(get_session)):
     image_file = await file.read()
     extracted_text = get_text_from_img(image_file)
     formatted_text = format_text(extracted_text)
-#  Cósigo para selecionar o tipo de id entraria nessa linha.
+
     nyx = Nyx(id_tipo= 1, 
               fields=formatted_text)
     nyx_data = NyxSQL(id_tipo= nyx.id_tipo,
@@ -35,4 +39,4 @@ async def read_img(file: Annotated[UploadFile, File(description='Upload an image
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app)
+    uvicorn.run('src.main:app')
